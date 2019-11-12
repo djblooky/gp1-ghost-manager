787a81fcb7904caeeb185e13d4c077dd1107d7b1
haley.phillips <haleyphillips928@gmail.com>
2019-11-12 13:57:29 -0600

    ghostmanager progress

--- GhostManager_HaleyPhillips/GhostManager.cs
+++ GhostManager_HaleyPhillips/GhostManager.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,17 +18,71 @@ namespace GhostManager_HaleyPhillips
 
     public class GhostManager : GameComponent, IGhostManager
     {
-        public List<Ghost> Ghosts { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string GhostTexture { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        List<Ghost> ghosts; 
+        public List<Ghost> Ghosts { get; set; }
+        List<Ghost> ghostsToRemove { get; set; }  //remove these ghosts when disabled
+        public string GhostTexture { get; set; }
+
+        protected Ghost g = null; //default ghost
+
+        public GhostManager(Game game) : base(game)
+        {
+            g = new Ghost(game);
+            ghosts = new List<Ghost>();
+            ghostsToRemove = new List<Ghost>();
+        }
 
         public Ghost ghost()
         {
-            throw new NotImplementedException();
+           
         }
 
         public Ghost ghost(Ghost g)
         {
-            throw new NotImplementedException();
+            
+        }
+
+        protected virtual void addGhost(Ghost g)
+        {
+            ghosts.Add(g);
+        }
+
+        protected virtual void removeGhost(Ghost g)
+        {
+            ghosts.Remove(g);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            ghostsToRemove.Clear(); //clear old ghosts to be removed
+
+            foreach (Ghost g in Ghosts)
+            {
+                if (g.Enabled)
+                {
+                    g.Update(gameTime); //Only update enabled ghosts
+                }
+                else //If the ghost is not enabled 
+                {
+                    ghostsToRemove.Add(g);
+                }
+            }
+
+            //Remove ghosts that are not enalbled anymore
+            foreach (Ghost s in ghostsToRemove)
+            {
+                this.removeGhost(s);
+            }
+            base.Update(gameTime);
+        }
+
+
+        public void Draw(SpriteBatch sb)
+        {
+            foreach (var g in Ghosts) //draw each ghost
+            {
+                 g.Draw(sb);
+            }
         }
     }
 }
