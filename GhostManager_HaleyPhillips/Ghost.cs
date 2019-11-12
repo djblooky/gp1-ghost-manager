126d110e0d0d8aa73b293a2f43d79958abbefa93
haley.phillips <haleyphillips928@gmail.com>
2019-11-12 13:48:57 -0600

    added ghost

---
+++ GhostManager_HaleyPhillips/Ghost.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGameLibrary.Sprite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostManager_HaleyPhillips
+{
+    public class Ghost : Sprite
+    {
+        float elapsedtime;
+        public Vector2 StartLocation { get; set; }
+        private string ghostTexture; //private instance data menber
+        public string GhostTexture
+        {
+            get { return ghostTexture; }
+            set
+            {
+                ghostTexture = value;
+                LoadContent();
+            }
+        }
+
+        public Ghost(Game game) : base(game)
+        {
+            if (String.IsNullOrEmpty(GhostTexture))
+            {
+                GhostTexture = "PurpleGhost";
+            }
+        }
+
+        protected override void LoadContent()
+        {
+            spriteTexture = Game.Content.Load<Texture2D>(GhostTexture);
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            elapsedtime = gameTime.ElapsedGameTime.Milliseconds;
+
+            if (Location == null)
+                Location = StartLocation;
+
+            Location += (Direction * Speed * (elapsedtime / 1000));
+
+            if (IsOffScreen())
+            {
+                Enabled = false;
+            }
+
+            base.Update(gameTime);
+        }
+    }
+}
---
+++ GhostManager_HaleyPhillips/GhostManager.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostManager_HaleyPhillips
+{
+    public interface IGhostManager
+    {
+        List<Ghost> Ghosts { get; set; }
+        Ghost ghost();
+        Ghost ghost(Ghost g);
+        string GhostTexture { get; set; }
+    }
+
+    public class GhostManager : GameComponent, IGhostManager
+    {
+        public List<Ghost> Ghosts { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string GhostTexture { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Ghost ghost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ghost ghost(Ghost g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
